2d6f75aed489c97f8cb6879e96f967952c2bc81a
facuforipa01 <facufolatelli2001@gmail.com>
2023-09-14 21:04:31 -0300

    tablas listas

--- controllers/post.js
+++ controllers/post.js
@@ -12,14 +12,14 @@ exports.getAllPosts = async (req, res) => {
     catch (error) {
         console.error(error);
         res.status(500).json({
-            message: 'Error al obtener los usuarios',
+            message: 'Error al obtener las Publicaciones',
         })
     }
 }
 
 exports.createPost = async (req, res) => {
     try {
-        const { idUser, descripcion, fecha } = req.body
+        const { descripcion, fecha, hora, imagen, ubicacion } = req.body
         const post = await Post.create(req.body);
         res.status(201).json({
             ok: true,
@@ -37,14 +37,14 @@ exports.createPost = async (req, res) => {
 exports.updatePost = async (req, res) => {
    
     const {id} = req.params
-    const {idUser, descripcion, fecha} = req.body
+    const {descripcion, fecha, hora, imagen, ubicacion } = req.body
 
     try {
 
         const post = await post.findOne({id});
 
         if(post){
-            post.update({idUser, descripcion, fecha})
+            post.update({descripcion, fecha, hora, imagen, ubicacion })
         }
         else{
             res.status(404).json({
--- models/post.js
+++ models/post.js
@@ -10,13 +10,13 @@ Post.init({
         primaryKey: true,
         autoIncrement: true
     },
-    idUser: {
+   /*// idUser: {
         type: DataTypes.INTEGER,
         forignKey: true, //no se si se define asi una clave foranea xd
         allowNull: false, // si o si la tiene que haber hecho alguien
         unique: false //pueden haber muchos posts de un mismo user
         //idUser es la clave primaria en usuarios que tiene que estar vinculada con posteos
-    },
+    },*/
     descripcion: {
         type: DataTypes.STRING,
         allowNull: true
@@ -25,7 +25,18 @@ Post.init({
         type: DataTypes.DATE,
         allowNull: false,
         unique: false
+    },
+    imagen: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+    },
+    ubicacion: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: false,
     }
+
 },
     {
         sequelize,
@@ -33,7 +44,7 @@ Post.init({
     });
 
 
-User.sync()
+Post.sync()
     .then(() => {
         console.log('La tabla de Publicaciones ha sido creada');
     })
